e65079924eb42dcf507066a503562b7f6b814075
Jason Parkin <jay-parkin91@hotmail.com>
2024-10-19 11:53:26 +1100

    feat: getuserbyid router and test implemented

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -18,8 +18,24 @@ router.post("/signup", async (request, response) => {
   // pretend we have written receivedUserData into a DB
   let placeholderDatabaseResult = { ...receivedUserData };
 
-  // receivedUserData is a placeholder for actually making a new user in a database
+  // placeholderDatabaseResult is a placeholder for actually making a new user in a database
   response.json(placeholderDatabaseResult);
+});
+
+// GET localhost:3000/users/12345
+router.get("/:userid", async (request, response) => {
+  let targetUserId = request.params.userid;
+  // let exampleSomeOtherProperty = request.params.someotherproperty;
+
+  // make a database query that uses that ID
+  // eg. let result = await User.findById(targetUserId);
+
+  response.json({
+    result: {
+      id: targetUserId,
+      username: "Pretend this is a username from the database",
+    },
+  });
 });
 
 module.exports = router;
