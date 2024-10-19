a3ada2ae366b36ab7e56ff45704abfe58f4400e4
Jason Parkin <jay-parkin91@hotmail.com>
2024-10-19 11:03:44 +1100

    feat: skip in user tests

--- tests/users.test.js
+++ tests/users.test.js
@@ -5,18 +5,18 @@ const { app } = require("../src/server.js");
 const request = require("supertest");
 
 describe("Users controller", () => {
-  test("Get all users route returns array of users", async () => {
+  test.skip("Get all users route returns array of users", async () => {
     // GET localhost:3000/users
     const response = await request(app).get("/users");
   });
 
-  test("Get user by ID route returns a single user as object", async () => {
+  test.skip("Get user by ID route returns a single user as object", async () => {
     // GET localhost:3000/users/12345
     let targetUserId = "12345";
     const response = await request(app).get("/users/" + targetUserId);
   });
 
-  test("User sign-up route returns a single user as object", async () => {
+  test.skip("User sign-up route returns a single user as object", async () => {
     // POST localhost:3000/users/signup
     const response = await request(app).post("/users/signup").send({
       username: "jason",
@@ -24,7 +24,7 @@ describe("Users controller", () => {
     });
   });
 
-  test("User login route returns a single user as object", async () => {
+  test.skip("User login route returns a single user as object", async () => {
     // POST localhost:3000/users/login
     const response = await request(app).post("/users/login").send({
       username: "jason",
@@ -32,7 +32,7 @@ describe("Users controller", () => {
     });
   });
 
-  test("User update/edit route returns a single user as object", async () => {
+  test.skip("User update/edit route returns a single user as object", async () => {
     // PATCH localhost:3000/users/12345
     const response = await request(app).patch("/users/12345").send({
       username: "jason",
@@ -40,7 +40,7 @@ describe("Users controller", () => {
     });
   });
 
-  test("User delete route returns return a number of users deleted", async () => {
+  test.skip("User delete route returns return a number of users deleted", async () => {
     // DELETE localhost:3000/users/12345
     const response = await request(app).delete("/users/12345").send({
       username: "jason",
